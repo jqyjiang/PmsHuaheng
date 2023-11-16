d93dd4f0fed2642d5a5a16d4c679d43ee0c1cc4b
yt <1875873909@qq.com>
2023-11-16 15:36:49 +0800

    第一次提交模版到仓库

---
+++ ruoyi-modules/hh-pms/src/main/java/com/hh/pms/HhPmsApplication.java
@@ -0,0 +1,35 @@
+package com.hh.pms;
+
+import com.ruoyi.common.security.annotation.EnableCustomConfig;
+import com.ruoyi.common.security.annotation.EnableRyFeignClients;
+import com.ruoyi.common.swagger.annotation.EnableCustomSwagger2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 采购管理系统模块
+ *
+ * @author yangtao
+ */
+@EnableCustomConfig
+@EnableCustomSwagger2
+@EnableRyFeignClients
+@SpringBootApplication
+public class HhPmsApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(HhPmsApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  采购管理系统模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+
+}
